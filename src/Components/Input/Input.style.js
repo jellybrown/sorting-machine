fc6b8a5cea67accc73074df14b850febd3e28f97
yujin mac <kimyu28jin@gmail.com>
2021-08-17 22:12:20 +0900

    Feat : 스타일 추가, 시계 추가

---
+++ src/Components/Input/Input.style.js
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+export const InputWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  position: relative;
+  width: 600px;
+  margin: 130px auto;
+`;
+
+export const TextInput = styled.input`
+  flex: 1;
+  outline: none;
+  border: none;
+  font-size: 14px;
+  color: #686868;
+  padding: 15px 15px;
+  border-radius: 6px;
+  margin-right: 15px;
+
+  &::placeholder {
+    color: #a4a4a4;
+  }
+  &::-webkit-input-placeholder {
+    color: #a4a4a4;
+  }
+  &:-ms-input-placeholder {
+    color: #a4a4a4;
+  }
+`;
+
+export const Button = styled.button`
+  border: none;
+  padding: 15px 35px;
+  background-color: #717171;
+  color: #fff;
+  font-size: 14px;
+  border-radius: 6px;
+  cursor: pointer;
+  transition: 0.3s;
+
+  &:hover {
+    background-color: #656565;
+  }
+`;
+
+export const ErrorMessage = styled.p`
+  font-size: 13px;
+  color: #dcdcdc;
+  position: absolute;
+  left: 4px;
+  bottom: -25px;
+`;
